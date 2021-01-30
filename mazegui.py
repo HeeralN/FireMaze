32318da005b310f0cb16d9ae8e91700888ed2792
vidyamannava <vidya.d.mannava@gmail.com>
2021-01-30 18:10:55 -0500

    maze filling method

---
+++ mazegui.py
@@ -0,0 +1,14 @@
+def maze(dim, p):
+    for x in range(dim+1):
+        for y in range(dim+1):
+            if(x==0 and y==0 or x==dim and y==dim):
+                continue
+            else: 
+                if(random.randrange()):
+                    print("Filled")
+                elif():
+                    print("Empty")
+
+
+
+
