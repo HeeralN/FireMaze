6c564f7f23a6efa1e119e2718dca4056271aad0b
vidyamannava <vidya.d.mannava@gmail.com>
2021-01-31 13:17:28 -0500

    updated generateMaze function

--- mazegui.py
+++ mazegui.py
@@ -1,13 +1,24 @@
-def maze(dim, p):
-    for x in range(dim+1):
-        for y in range(dim+1):
-            if(x==0 and y==0 or x==dim and y==dim):
+def generateMaze(dim, p):
+    maze = []
+    for x in range(dim):
+        currRow = []
+        for y in range(dim):
+            if(x==0 and y==0):
+                print("S", end =" ")
                 continue
+            elif(x==dim-1 and y==dim-1):
+                print("G", end =" ")
             else: 
-                if(random.randrange()):
-                    print("Filled")
-                elif():
-                    print("Empty")
+                #if():
+                    currRow.append("|")
+                    print("|", end =" ")
+                #elif():
+                #    print("Empty")
+        maze.append(currRow)
+        print()
+
+
+generateMaze(5, 0.25)
 
 
 
